632d34cae5223de24d3030b198b852fe9afae362
d-sanchez <danielsanchez@nevada.unr.edu>
2017-01-30 15:08:09 -0800

    analytics

--- index.php
+++ index.php
@@ -24,6 +24,17 @@
     <!-- CSS -->
     <link href="stylesheets/style.css" rel="stylesheet">
 
+    <!-- Google Analytics -->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-91173305-1', 'auto');
+      ga('send', 'pageview');
+    </script>
+
 </head>
 
 
